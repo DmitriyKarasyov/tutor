42979ce5de420ff43994dd1f27d1167b8db39674
Nightcall <bini2@yandex.ru>
2024-04-21 23:06:07 +0300

    add schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS meeting_points (
+
+);
+
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(30) NOT NULL,
+    telephone_number VARCHAR(30),
+    subject VARCHAR(30),
+    grade INTEGER,
+
+)
